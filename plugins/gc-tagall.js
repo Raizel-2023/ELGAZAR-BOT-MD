3a83d17343dbe860bf46a3296018ed4bdba4c0df
Raizel-2023 <111248812+Raizel-2023@users.noreply.github.com>
2022-12-25 18:25:20 +0200

    Add files via upload

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -4,16 +4,16 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
-let teks = `*⺀𝙸 𝙽 𝚅 𝙾 𝙲 𝙰 𝙽 𝙳 𝙾 - 𝙶 𝚁 𝚄 𝙿 𝙾⺀*\n\n❏ ${oi}\n\n❏ *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
+let oi = `*الرساله:* ${pesan}`
+let teks = `*⺀منشن للكل🗿⺀*\n\n❏ ${oi}\n\n❏ *قائمه الورعان🗿:*\n`
 for (let mem of participants) {
 teks += `┣➥ @${mem.id.split('@')[0]}\n`}
-teks += `*└* 𝐁𝐲 𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭\n\n*▌│█║▌║▌║║▌║▌║▌║█*`
+teks += `*𝘌𝘓Ｇ𝘈𝘡𝘈𝘙 𝘉𝘖𝘛❤*\n\n*▌│█║▌║▌║║▌║▌║▌║█*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
 handler.tags = ['group']
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+handler.command = /^(tagall|منشن|invocacion|todos|تاك)$/i
 handler.admin = true
 handler.group = true
 export default handler
